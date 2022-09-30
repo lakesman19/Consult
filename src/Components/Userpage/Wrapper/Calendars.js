3af34d3ed193a655556f5d6accb6d727907d7166
lakesman19 <olalekanonauga@gmail.com>
2022-09-30 01:31:20 +0100

    updated

--- src/Components/Userpage/Wrapper/Calendars.js
+++ src/Components/Userpage/Wrapper/Calendars.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Wrapper from '../Wrapper'
-
+import {
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Year,
+  TimelineViews,
+  TimelineMonth,
+  TimelineYear,
+  ViewsDirective,
+  ViewDirective,
+  ResourcesDirective,
+  ResourceDirective,
+  Inject,
+  Resize,
+  DragAndDrop,
+  Agenda,
+  Print,
+  ExcelExport,
+  ICalendarImport,
+  ICalendarExport,
+  Timezone,
+} from '@syncfusion/ej2-react-schedule'
 
 const Calendars = () => {
 
   return (
     <Wrapper>
       <>
-
+        <ScheduleComponent currentView='Month' className='w-100'>
+          <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda]}/>
+        </ScheduleComponent>
       </>
     </Wrapper>
   )
